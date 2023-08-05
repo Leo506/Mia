4d9f48d0b91a397b92eef87fdc73bbe14157e01e
Artem Levin <leoartemlev@yandex.ru>
2023-08-05 13:01:58 +0500

    remove Web API

--- RssReader.Domain/Abstractions/IArticlesRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using RssReader.Domain.Models;
-
-namespace RssReader.Domain.Abstractions;
-
-public interface IArticlesRepository
-{
-    Task<IEnumerable<Article>> GetAll(string feed);
-}
--- RssReader.Domain/Abstractions/IArticlesService.cs
+++
@@ -1,8 +0,0 @@
-using RssReader.Domain.Models;
-
-namespace RssReader.Domain.Abstractions;
-
-public interface IArticlesService
-{
-    Task<IEnumerable<Article>> GetArticles(string feed);    
-}
